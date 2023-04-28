69a2704d4774d7fa9b48005572677ac2cafa008d
VITOR PAVANELLO <vitorpavanello98@gmail.com>
2023-04-28 13:50:02 -0300

    subindo back

---
+++ RastreioPacote/Controllers/RastreioPacoteController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RastreioPacote.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RastreioPacoteController : ControllerBase
+    {
+        private readonly ILogger<RastreioPacoteController> _logger;
+
+        public RastreioPacoteController(ILogger<RastreioPacoteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "RastrearPacote")]
+        public List<RastreioPacoteReturn> Post([FromQuery] string numero)
+        {
+            try
+            {
+                var result = new Correios.NET.CorreiosService().GetPackageTracking(numero);
+
+                if (result is null)
+                {
+                    return new List<RastreioPacoteReturn>();
+                }
+
+                var returnList = new List<RastreioPacoteReturn>();
+
+                foreach (var track in result.TrackingHistory)
+                {
+                    var historico = new RastreioPacoteReturn();
+
+                    historico.Date = track.Date.ToString("dd/MM/yyyy hh:mm tt");
+                    historico.Source = track.Source;
+                    historico.Status = track.Status;
+
+                    returnList.Add(historico);
+                }
+
+                return returnList;
+            }
+            catch(Exception ex)
+            {
+                return new List<RastreioPacoteReturn>();
+            }
+            
+        }
+    }
+}
---
+++ RastreioPacote/RastreioPacoteReturn.cs
@@ -0,0 +1,9 @@
+﻿namespace RastreioPacote
+{
+    public class RastreioPacoteReturn
+    {
+        public string Date { get; set; }
+        public string Source { get; set; }
+        public string Status { get; set; }
+    }
+}
